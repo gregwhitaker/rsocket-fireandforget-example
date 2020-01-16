80f34978664bc256896d6487551fa42560b88b69
gregwhitaker <gwhitake@gmail.com>
2020-01-16 15:49:57 -0800

    Doing work

--- count-service/src/main/java/example/count/service/CountService.java
+++ count-service/src/main/java/example/count/service/CountService.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CountService {
-    private static final Logger LOG = LoggerFactory.getLogger(CountService.class)
+    private static final Logger LOG = LoggerFactory.getLogger(CountService.class);
 
     public static void main(String... args) {
 
